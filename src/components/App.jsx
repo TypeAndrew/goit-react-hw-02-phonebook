600e6447317433d83ff97023660c4d094326e6d9
Andrew <andy.shular@gmail.com>
2022-12-23 19:13:49 +0200

    second task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import { Component } from "react";
+
 export const App = () => {
   return (
     <div
@@ -10,7 +12,56 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <Phonebook />
+      <Contacts />
     </div>
   );
 };
+
+class Phonebook extends Component {
+  
+  state = {
+    contacts: [],
+    name: ''
+  }
+
+  handleChange(){
+
+  }
+
+  handleSubmit = (evt)=>{
+    console.log(evt.target[0].value);
+    this.setState({name: evt.target[0].value});
+  }
+
+  render(){
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" name="User" />
+       
+        <button type="submit">Add contact</button>
+      </form>
+    )
+   } 
+  }
+
+  class Contacts extends Component {
+   
+    handleChange(){
+  
+    }
+  
+    handleSubmit(){
+  
+    }
+  
+    render(){
+      return(
+        <ul>
+          <li>++++</li>
+        </ul>
+        
+      )
+     } 
+    }
+  
