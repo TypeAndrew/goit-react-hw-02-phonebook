a10fdbf786fe8553169238a4a4ccf09753ecf623
TypeAndrew <andy.shular@gmail.com>
2022-12-27 15:16:14 +0200

    git commit task 2

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,13 +1,14 @@
 import PropTypes from 'prop-types';
 
 export function Filter(props)  {
-    const { handleFilter } = props; 
+    const { handleFilter, filter } = props; 
     
     return (
       <label > Filter
           <input onChange={handleFilter}
           type="text"
           name="filter"
+          value={filter}
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           />
